a8b0e56c7d6ab290f3ac09f8fa5c3712a0a0eb98
albirddd <49213768+albirddd@users.noreply.github.com>
2020-10-08 11:27:53 -0700

    Add files via upload

---
+++ food/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace food
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.FoodName = new System.Windows.Forms.Label();
+            this.calories = new System.Windows.Forms.TextBox();
+            this.carbs = new System.Windows.Forms.TextBox();
+            this.protein = new System.Windows.Forms.TextBox();
+            this.fats = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(444, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(93, 22);
+            this.toolStripMenuItem1.Text = "Exit";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(12, 60);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(348, 20);
+            this.Name.TabIndex = 1;
+            this.Name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // FoodName
+            // 
+            this.FoodName.AutoSize = true;
+            this.FoodName.Location = new System.Drawing.Point(12, 44);
+            this.FoodName.Name = "FoodName";
+            this.FoodName.Size = new System.Drawing.Size(91, 13);
+            this.FoodName.TabIndex = 2;
+            this.FoodName.Text = "Insert Food Name";
+            // 
+            // calories
+            // 
+            this.calories.Location = new System.Drawing.Point(15, 147);
+            this.calories.Name = "calories";
+            this.calories.Size = new System.Drawing.Size(127, 20);
+            this.calories.TabIndex = 3;
+            // 
+            // carbs
+            // 
+            this.carbs.Location = new System.Drawing.Point(233, 215);
+            this.carbs.Name = "carbs";
+            this.carbs.Size = new System.Drawing.Size(127, 20);
+            this.carbs.TabIndex = 4;
+            // 
+            // protein
+            // 
+            this.protein.Location = new System.Drawing.Point(233, 147);
+            this.protein.Name = "protein";
+            this.protein.Size = new System.Drawing.Size(127, 20);
+            this.protein.TabIndex = 5;
+            // 
+            // fats
+            // 
+            this.fats.Location = new System.Drawing.Point(15, 215);
+            this.fats.Name = "fats";
+            this.fats.Size = new System.Drawing.Size(127, 20);
+            this.fats.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Calories";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Carbohydrates";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Protein";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Fats";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Allergies";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Gluten",
+            "Dairy",
+            "Soy",
+            "Peanut",
+            "Fish"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 290);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(207, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(15, 326);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(186, 112);
+            this.Add.TabIndex = 14;
+            this.Add.Text = "Add Food";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 450);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fats);
+            this.Controls.Add(this.protein);
+            this.Controls.Add(this.carbs);
+            this.Controls.Add(this.calories);
+            this.Controls.Add(this.FoodName);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Food";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.Label FoodName;
+        private System.Windows.Forms.TextBox calories;
+        private System.Windows.Forms.TextBox carbs;
+        private System.Windows.Forms.TextBox protein;
+        private System.Windows.Forms.TextBox fats;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Add;
+    }
+}
+
---
+++ food/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace food
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Allergies_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+                IDictionary<string, string> register = new Dictionary<string, string>();
+                register['FoodName'] = FoodName.Text;
+                register['Calories'] = calories.Text;
+                register['Protein'] = protein.Text;
+                register['Carbohydrates'] = carbs.Text;
+                register['Fats'] = fats.Text;
+                register['Allergies'] = Allergies_Enter;
+            //Connect to database 
+
+            Database p = new Database();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
