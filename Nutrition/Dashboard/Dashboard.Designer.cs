c7add012f522ff20a8bb13336b490415c338dd8c
EvanaHolevas <70241021+EvanaHolevas@users.noreply.github.com>
2020-11-05 22:39:40 -0800

    Changed buttons to tabs. Added graphs

--- Nutrition/Dashboard/Dashboard.Designer.cs
+++ Nutrition/Dashboard/Dashboard.Designer.cs
@@ -28,81 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.recipeSearch = new System.Windows.Forms.ToolStripMenuItem();
-            this.addFood = new System.Windows.Forms.ToolStripMenuItem();
-            this.Graphs = new System.Windows.Forms.ToolStripMenuItem();
-            this.weekly = new System.Windows.Forms.ToolStripMenuItem();
-            this.monthly = new System.Windows.Forms.ToolStripMenuItem();
-            this.yearly = new System.Windows.Forms.ToolStripMenuItem();
+            this.components = new System.ComponentModel.Container();
             this.statusBar = new System.Windows.Forms.StatusStrip();
             this.dashboardUser = new System.Windows.Forms.ToolStripDropDownButton();
             this.logoutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dateLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.lastLoginLabel = new System.Windows.Forms.ToolStripLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.weightFormsPlot = new ScottPlot.FormsPlot();
+            this.barsFormsPlot = new ScottPlot.FormsPlot();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.menuStrip1.SuspendLayout();
             this.statusBar.SuspendLayout();
             this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.recipeSearch,
-            this.addFood,
-            this.Graphs});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(966, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
-            // 
-            // recipeSearch
-            // 
-            this.recipeSearch.Name = "recipeSearch";
-            this.recipeSearch.Size = new System.Drawing.Size(92, 20);
-            this.recipeSearch.Text = "Recipe Search";
-            // 
-            // addFood
-            // 
-            this.addFood.Name = "addFood";
-            this.addFood.Size = new System.Drawing.Size(76, 20);
-            this.addFood.Text = "Add Foods";
-            // 
-            // Graphs
-            // 
-            this.Graphs.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.weekly,
-            this.monthly,
-            this.yearly});
-            this.Graphs.Name = "Graphs";
-            this.Graphs.Size = new System.Drawing.Size(108, 20);
-            this.Graphs.Text = "Health Summary";
-            // 
-            // weekly
-            // 
-            this.weekly.Name = "weekly";
-            this.weekly.Size = new System.Drawing.Size(119, 22);
-            this.weekly.Text = "Weekly";
-            // 
-            // monthly
-            // 
-            this.monthly.Name = "monthly";
-            this.monthly.Size = new System.Drawing.Size(119, 22);
-            this.monthly.Text = "Monthly";
-            // 
-            // yearly
-            // 
-            this.yearly.Name = "yearly";
-            this.yearly.Size = new System.Drawing.Size(119, 22);
-            this.yearly.Text = "Yearly";
             // 
             // statusBar
             // 
@@ -111,10 +57,11 @@
             this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.dashboardUser,
             this.dateLabel});
-            this.statusBar.Location = new System.Drawing.Point(0, 24);
+            this.statusBar.Location = new System.Drawing.Point(3, 3);
             this.statusBar.Name = "statusBar";
-            this.statusBar.Size = new System.Drawing.Size(966, 36);
+            this.statusBar.Size = new System.Drawing.Size(952, 36);
             this.statusBar.TabIndex = 2;
+            this.statusBar.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusBar_ItemClicked);
             // 
             // dashboardUser
             // 
@@ -140,7 +87,7 @@
             // 
             this.dateLabel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.dateLabel.Name = "dateLabel";
-            this.dateLabel.Size = new System.Drawing.Size(910, 31);
+            this.dateLabel.Size = new System.Drawing.Size(896, 31);
             this.dateLabel.Spring = true;
             this.dateLabel.Text = "CurrentDate";
             this.dateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -150,10 +97,10 @@
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.lastLoginLabel});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 615);
+            this.toolStrip1.Location = new System.Drawing.Point(3, 584);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolStrip1.Size = new System.Drawing.Size(966, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(952, 25);
             this.toolStrip1.TabIndex = 3;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -165,86 +112,136 @@
             this.lastLoginLabel.Text = "Last Login";
             this.lastLoginLabel.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 3;
+            this.tabControl1.Size = new System.Drawing.Size(966, 640);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.weightFormsPlot);
+            this.tabPage1.Controls.Add(this.barsFormsPlot);
+            this.tabPage1.Controls.Add(this.toolStrip1);
+            this.tabPage1.Controls.Add(this.statusBar);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(958, 612);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Dashboard";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // weightFormsPlot
+            // 
+            this.weightFormsPlot.Location = new System.Drawing.Point(481, 42);
+            this.weightFormsPlot.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.weightFormsPlot.Name = "weightFormsPlot";
+            this.weightFormsPlot.Size = new System.Drawing.Size(472, 232);
+            this.weightFormsPlot.TabIndex = 5;
+            this.weightFormsPlot.Load += new System.EventHandler(this.formsPlot1_Load);
+            // 
+            // barsFormsPlot
+            // 
+            this.barsFormsPlot.Location = new System.Drawing.Point(4, 42);
+            this.barsFormsPlot.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.barsFormsPlot.Name = "barsFormsPlot";
+            this.barsFormsPlot.Size = new System.Drawing.Size(472, 232);
+            this.barsFormsPlot.TabIndex = 4;
+            this.barsFormsPlot.Load += new System.EventHandler(this.barsFormsPlot_Load_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(958, 612);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Add Food";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(958, 612);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Recipe Search";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(958, 612);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Health Summary";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(5, 95);
+            this.groupBox1.Location = new System.Drawing.Point(484, 288);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(446, 246);
-            this.groupBox1.TabIndex = 4;
+            this.groupBox1.Size = new System.Drawing.Size(468, 288);
+            this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Daily Progress Graph";
+            this.groupBox1.Text = "Food Quickview";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(471, 95);
+            this.groupBox2.Location = new System.Drawing.Point(8, 288);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(483, 246);
-            this.groupBox2.TabIndex = 5;
+            this.groupBox2.Size = new System.Drawing.Size(468, 288);
+            this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Weight Change Graph";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Location = new System.Drawing.Point(5, 362);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(446, 240);
-            this.groupBox3.TabIndex = 6;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Promoted Recipe";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Location = new System.Drawing.Point(470, 362);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(484, 240);
-            this.groupBox4.TabIndex = 7;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Quick View Foods";
+            this.groupBox2.Text = "Recipe?";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(966, 640);
-            this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.statusBar);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.tabControl1);
             this.Name = "Dashboard";
             this.Text = "Nutrition Dashboard";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.statusBar.ResumeLayout(false);
             this.statusBar.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem Graphs;
-        private System.Windows.Forms.ToolStripMenuItem weekly;
-        private System.Windows.Forms.ToolStripMenuItem monthly;
-        private System.Windows.Forms.ToolStripMenuItem yearly;
         private System.Windows.Forms.StatusStrip statusBar;
-        private System.Windows.Forms.ToolStripMenuItem recipeSearch;
-        private System.Windows.Forms.ToolStripMenuItem addFood;
         private System.Windows.Forms.ToolStripDropDownButton dashboardUser;
         private System.Windows.Forms.ToolStripMenuItem logoutMenuItem;
         private System.Windows.Forms.ToolStripStatusLabel dateLabel;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripLabel lastLoginLabel;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private ScottPlot.FormsPlot barsFormsPlot;
+        private ScottPlot.FormsPlot weightFormsPlot;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.GroupBox groupBox4;
     }
 }
--- Nutrition/Dashboard/Dashboard.cs
+++ Nutrition/Dashboard/Dashboard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScottPlot;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace Nutrition
     {
         private string username;
         private IDictionary<string, string> userData;
-        private DateTime join_date;
+        private DateTime join_date; //FIXME: what is this needed for?
         private DateTime last_login;
 
         public Dashboard(IDictionary<string, string> user)
@@ -31,10 +32,11 @@ namespace Nutrition
                 data["last_login"]
             */
             InitializeComponent();
-            dashboardUser.Text = "Welcome " + getUser();
+            dashboardUser.Text = "Welcome, " + getUser();
             lastLoginLabel.Text = "You last logged in " + getLastLogin();
             dateLabel.Text = DateTime.Now.ToString();
         }
+
 
         public string getUser()
         {
@@ -67,6 +69,68 @@ namespace Nutrition
                 double fat = bb.sumMacroData(this.username, "fat");
                 MessageBox.Show("Protein Sum: " + protein +"\nFat Sum: " + fat);
             }
+        }
+
+        private void statusBar_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void barsFormsPlot_Load_1(object sender, EventArgs e)
+        {
+            //needs to get from database - either calories from each or grams
+            double[] y1Recomended = { 60, 35, 35 };
+            double[] y1Below = { 44, 9, 19 };
+            double[] y2Actual = { 60, 15, 40 };
+            double[] xMacros = { 1, 2, 3 };
+
+            // plot the data
+            barsFormsPlot.Reset();
+
+            //should generate "recomended range" based on user BMR
+            //FIXME: make colors nicer
+            barsFormsPlot.plt.PlotBar(xMacros, y1Recomended, null, "Recomended Range", barWidth: .3, xOffset: -.2);
+            //below range
+            barsFormsPlot.plt.PlotBar(xMacros, y1Below, null, null, barWidth: .3, xOffset: -.2);
+
+            //User's entered macros
+            barsFormsPlot.plt.PlotBar(xMacros, y2Actual, null, "Actual", barWidth: 0.3, xOffset: .2);
+
+            string[] labels = { "Carbs", "Protein", "Fats" };
+            barsFormsPlot.plt.Axis(y1: 0);
+            barsFormsPlot.plt.Grid(enableVertical: false, lineStyle: LineStyle.Dot);
+            barsFormsPlot.plt.Axis(y1: 0);
+            barsFormsPlot.plt.Legend(location: legendLocation.upperRight);
+            barsFormsPlot.plt.XTicks(xMacros, labels); //labels the 3 bars
+
+            barsFormsPlot.Render();
+        }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+            //clear previous data
+            weightFormsPlot.Reset();
+
+            //FIXME, not sure how to structure this, needs to get from database
+            //autofill with 0 if data not avalible 
+            double[] days = { 1, 2, 3, 4, 5, 6, 7 };
+
+            //needs to get the user entered data from database
+            //autofill with 0 if data not avalible 
+            double[] userWeight = { 130, 130, 128, 127, 129, 127, 126 };
+            weightFormsPlot.plt.PlotScatter(days, userWeight);
+
+            //Label x and y axis
+            weightFormsPlot.plt.XLabel("Days");
+            weightFormsPlot.plt.YLabel("Pounds");
+
+            weightFormsPlot.Render();
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
