83b487eb6992f65cca8613751e178f67661dfe9b
kashuck <60717466+kashuck@users.noreply.github.com>
2020-11-12 15:41:46 -0800

    Add files via upload

---
+++ Team9Repo-master/Nutrition/FoodEntry/FoodEntry.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Nutrition
+{
+    class FoodEntry
+    {
+        IDictionary<string, Food> items; //<food name, food object>
+        public FoodEntry()
+        {
+            items = new Dictionary<string, Food>();
+        }
+
+        public void addNewFood(Food f)//adds new food list with item if list does not already exist
+        {
+            if (!items.ContainsKey(f.name))
+            {
+                items.Add(f.name, f);
+            }
+        }
+        void addDuplicateFood(Food f)//adds duplicate item to the list of the food if already exists
+        {
+            if (items.ContainsKey(f.name))
+            {
+                items.Add(f.name, f);
+            }
+        }
+
+        void removeAllFood(string f)//remove entire list with specific food name
+        {
+            items.Remove(f);
+        }
+        void removeSingleFood(Food f)//removes single specific instance of item from list with that name
+        {
+            if (items.ContainsKey(f.name))
+            {
+                items.Remove(f.name);
+            }
+        }
+
+        public Food searchByName(string s) //searches by name of the food (key)
+        {
+            if (!items.ContainsKey(s))
+            {
+                return null;
+            }
+            else
+            {
+                return items[s];
+            }
+        }
+        public bool updateItem(Food r, Food f) //replace at name 'r' with object 'f' if they share a name
+        {
+            if(items.ContainsKey(r.name))
+            {
+                items[r.name] = f;
+                return true;
+            }
+            return false;
+        }
+
+        public List<Food> printAll()  //put all names of food into a list and return the list
+        {
+            List<Food> allItems = new List<Food>();
+            foreach (var entry in items)
+            {
+                allItems.Add(entry.Value);
+            }
+            return allItems;
+        }
+    }
+}
---
+++ Team9Repo-master/Nutrition/Goals/NutritionGoals.cs
@@ -0,0 +1,65 @@
+﻿using Nutrition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nutrition
+{//TODO: If losing/gaining/maintaining do something
+    class NutritionGoals
+    {
+        private int bmi, bmr;
+        private string username;
+
+        public enum Status { Maintenance, Lose_it, Gain };
+        public NutritionGoals(string username, Status status)
+        {
+            this.username = username;
+            //Pull BMI/BMR from DB
+            Database d = new Database();
+            bmr = d.GetBMR(username);
+            bmi = d.GetBMI(username);
+        }
+
+        //Set a goal and save it to the database
+        public void setGoal(int calories)
+        {
+            Database d = new Database();
+            d.SetGoal(username, calories);
+        }
+
+        //Return a percentage value without the % sign
+        //TODO: Use this with the graph class
+        public int getGoalProgress()
+        {
+            Database d = new Database();
+            int total = d.GetGoal(username);
+
+            double current = 0;//d.sumMacroData(username, "calories");
+            if (total == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return (int)((current / total) * 100);
+            }
+        }
+
+
+        //Get & Set BMI
+        public int BMI
+        {
+            get { return bmi; }
+            set { bmi = value; }
+        }
+
+        //Get & Set BMR
+        public int BMR
+        {
+            get { return bmr; }
+            set { bmr = value; }
+        }
+
+    }
+}
