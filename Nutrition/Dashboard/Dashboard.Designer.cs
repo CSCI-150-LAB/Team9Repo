5c38c9dc9b441b7c63ada0b1f31d4c77e9b27dc9
Kyle A <kyleandrews@mail.fresnostate.edu>
2020-11-17 06:43:19 +0000

    Update Dashboard.Designer.cs

--- Nutrition/Dashboard/Dashboard.Designer.cs
+++ Nutrition/Dashboard/Dashboard.Designer.cs
@@ -256,6 +256,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.ShowEditingIcon = false;
             this.dataGridView1.Size = new System.Drawing.Size(190, 196);
             this.dataGridView1.TabIndex = 7;
